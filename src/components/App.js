847397da72a71c206f041dc3f12c067a1ff2b630
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-15 14:14:59 -0500

    Create basic file and folders structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import BooksForm from './BooksForm';
+import BooksList from './BooksList';
 
-const App = () => (<div>hello</div>);
+const App = () => (
+  <div>
+    <BooksList />
+    <BooksForm />
+  </div>
+);
 
 export default App;
