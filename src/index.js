00aefba3505575f54c160ad93ffd50cc9b4a5499
Ceci007 <cecibenitezca@gmail.com>
2021-02-15 18:53:47 -0300

    Add BooksForm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import rootReducer from './reducers/books';
 
