0edda0311e44abb40b2639121048f00a65e03c82
kenderb <kenderbolivar@gmail.com>
2021-02-15 15:51:58 -0500

    Create a templay to display the books

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Books from './Books';
+// import bookReducer from '../reducers';
 
-const BooksList = () => (
-  <table>
-    <tr>
-      <td>id</td>
-      <td>title</td>
-      <td>category</td>
-    </tr>
-  </table>
-);
+const BooksList = () => {
+  const bookList = [{ id: 1, title: 'some', category: 'Action' }];
+  return (
+    <table>
+      <tr>
+        <td>id</td>
+        <td>title</td>
+        <td>category</td>
+      </tr>
+      {bookList.map(book => (
+        <Books
+          id={book.id}
+          key={book.id}
+          title={book.title}
+          category={book.category}
+        />
+      ))}
+    </table>
+  );
+};
 
 export default BooksList;
