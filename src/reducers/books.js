af6ed414dac8925438ec1465ded0cd84f1b4e440
kenderb <kenderbolivar@gmail.com>
2021-02-16 10:30:54 -0500

    implement the books reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,2 +1,31 @@
-const bookReducer = () => ('book');
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
+
+const initialState = {
+  books: [
+    {
+      id: Math.random() * 10,
+      title: 'Book 1',
+      category: 'A',
+    },
+    {
+      id: Math.random() * 10,
+      title: 'Book 2',
+      category: 'B',
+    },
+  ],
+};
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return [
+        ...state,
+        action.id,
+      ];
+    case REMOVE_BOOK:
+      return state.filter(book => book.id !== action.id);
+    default:
+      return state;
+  }
+};
 export default bookReducer;
