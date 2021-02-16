d65f02731f15c679af61fbd3ceb4993846ed1ec9
Ceci007 <cecibenitezca@gmail.com>
2021-02-16 18:17:49 -0300

    Add useState to input

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const categoryList = [
   'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi',
 ];
 
-const BooksForm = () => (
-  <form>
-    <p>Title</p>
-    <input type="text" name="title" id="t" />
-    <p>Category</p>
-    <select name="categories" id="categories">
-      {categoryList.map(category => (<option key={category} value={category}>{category}</option>))}
-    </select>
-  </form>
-);
+const BooksForm = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState(categoryList[0]);
+
+  const handleChange = event => {
+    if (event.target.name === 'title') {
+      setTitle({ title: event.target.value });
+      console.log(title);
+    } else if (event.target.name === 'category') {
+      setCategory({ category: event.target.value });
+      console.log(category);
+    }
+  };
+
+  return (
+    <form>
+      <p>Title</p>
+      <input type="text" name="title" onChange={event => handleChange(event)} />
+      <p>Category</p>
+      <select value={category} name="categories" onChange={event => handleChange(event)}>
+        {categoryList.map(cat => (
+          <option key={cat}>
+            {cat}
+          </option>
+        ))}
+      </select>
+    </form>
+  );
+};
 
 export default BooksForm;
