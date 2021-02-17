57c7c54c4237e693b8e053a94fbab13b8f879645
Ceci007 <cecibenitezca@gmail.com>
2021-02-17 13:49:06 -0300

    Add filter action and filter reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
+export const CHANGE_FILTER = 'CHANGE_FILTER';
 
 export const createBook = book => ({
   type: CREATE_BOOK,
@@ -9,4 +10,9 @@ export const createBook = book => ({
 export const removeBook = id => ({
   type: REMOVE_BOOK,
   payload: id,
+});
+
+export const changeFilter = category => ({
+  type: CHANGE_FILTER,
+  payload: category
 });
